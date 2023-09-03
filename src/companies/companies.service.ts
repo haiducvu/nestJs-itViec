67f1945d5b94a75961c91348e936ea97899a29db
Hai Vu <duchaivu1997@gmail.com>
2023-09-04 00:31:29 +0700

    Delete Company

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -44,7 +44,18 @@ export class CompaniesService {
     );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+  async remove(id: string, user: IUser) {
+    await this.companyModel.updateOne(
+      { _id: id },
+      {
+        deleteBy: {
+          _id: user._id,
+          email: user.email,
+        },
+      },
+    );
+    return this.companyModel.softDelete({
+      _id: id,
+    });
   }
 }
