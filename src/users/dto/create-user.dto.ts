d76a62dc477ac9c48abc59f747e8501991bfcdd4
Hai Vu <duchaivu1997@gmail.com>
2023-09-09 17:07:08 +0700

    User Schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,18 +1,68 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  ValidateNested,
+} from 'class-validator';
+import mongoose from 'mongoose';
+class Company {
+  @IsNotEmpty()
+  _id: mongoose.Schema.Types.ObjectId;
+  @IsNotEmpty()
+  name: string;
+}
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Name không được bỏ trống' })
+  name: string;
   @IsEmail(
     {},
     {
-      message: 'Empty email',
+      message: 'Email không đúng định dạng',
     },
   )
   @IsNotEmpty({
-    message: 'Empty password',
+    message: 'Email không được bỏ trống',
   })
   email: string;
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Password không được bỏ trống' })
   password: string;
+  @IsNotEmpty({ message: 'Age không được bỏ trống' })
+  age: number;
+  @IsNotEmpty({ message: 'Gender không được bỏ trống' })
+  gender: number;
+  @IsNotEmpty({ message: 'Address không được bỏ trống' })
+  address: number;
+  @IsNotEmpty({ message: 'Role không được bỏ trống' })
+  role: string;
+
+  @IsNotEmptyObject()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => Company)
+  company: Company;
+}
+
+export class RegisterUserDto {
+  @IsNotEmpty({ message: 'Name không được bỏ trống' })
   name: string;
-  address: string;
+  @IsEmail(
+    {},
+    {
+      message: 'Email không đúng định dạng',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email không được bỏ trống',
+  })
+  email: string;
+  @IsNotEmpty({ message: 'Password không được bỏ trống' })
+  password: string;
+  @IsNotEmpty({ message: 'Age không được bỏ trống' })
+  age: number;
+  @IsNotEmpty({ message: 'Gender không được bỏ trống' })
+  gender: number;
+  @IsNotEmpty({ message: 'Address không được bỏ trống' })
+  address: number;
 }
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 @Schema({ timestamps: true })
@@ -10,12 +10,35 @@ export class User {
   password: string;
   @Prop()
   name: string;
+  @Prop({ type: Object })
+  company: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+  };
   @Prop()
-  phone: string;
+  role: string;
+  @Prop()
+  refreshToken: string;
   @Prop()
   age: string;
   @Prop()
+  gender: string;
+  @Prop()
   address: string;
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
   @Prop()
   createAt: Date;
   @Prop()
