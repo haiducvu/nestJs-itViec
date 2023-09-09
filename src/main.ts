d56acc6052398da160144715ec0c1d9abdd5d4e7
Hai Vu <duchaivu1997@gmail.com>
2023-09-09 14:48:46 +0700

    Version APIs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 
@@ -22,6 +22,14 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
+
+  // config versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'],
+  });
+
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
