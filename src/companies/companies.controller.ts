53d52ddb20a8a56e061167fb3efb2a0fd4b164cd
Hai Vu <duchaivu1997@gmail.com>
2023-09-14 00:12:52 +0700

    Make consistent with FE

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -29,8 +29,8 @@ export class CompaniesController {
   @Get()
   @ResponseMessage('Fetch List Company with Pagination')
   findAll(
-    @Query('page') currentPage: string,
-    @Query('limit') limit: string,
+    @Query('current') currentPage: string,
+    @Query('pageSize') limit: string,
     @Query() qs: string,
   ) {
     return this.companiesService.findAll(currentPage, limit, qs);
--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -27,8 +27,8 @@ export class CompaniesService {
 
   async findAll(currentPage: string, limit: string, qs: string) {
     const { filter, sort, projection, population } = aqp(qs);
-    delete filter.page;
-    delete filter.limit;
+    delete filter.current;
+    delete filter.pageSize;
 
     let offset = (+currentPage - 1) * +limit;
     let defaultLimit = +limit ? +limit : 10;
