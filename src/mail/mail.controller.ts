f43d47372af9d3d3ec91beef2421c4544c911bc8
Hai Vu <duchaivu1997@gmail.com>
2023-10-25 23:33:55 +0700

    Dynamic template for email

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -2,23 +2,57 @@ import { Controller, Get } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailerService } from '@nestjs-modules/mailer';
 import { Public, ResponseMessage } from 'src/decorator/customize';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import {
+  Subscriber,
+  SubscriberDocument,
+} from 'src/subscribers/schemas/subscriber.schema';
+import { Job, JobDocument } from 'src/jobs/schemas/job.schema';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Controller('mail')
 export class MailController {
   constructor(
     private readonly mailService: MailService,
     private mailerService: MailerService,
+    @InjectModel(Subscriber.name)
+    private subscriberModel: SoftDeleteModel<SubscriberDocument>,
+    @InjectModel(Job.name)
+    private jobModel: SoftDeleteModel<JobDocument>,
   ) {}
 
   @Get()
   @Public()
   @ResponseMessage('Test email')
   async handleTestEmail() {
-    await this.mailerService.sendMail({
-      to: 'duchaivu1997@gmail.com',
-      from: '"Support Team" <support@example.com>', // override default from
-      subject: 'Welcome to Nice App! Confirm your Email',
-      template: 'job', //'<b>welcome bla bla</b>', // HTML body content
-    });
+    const subscribers = await this.subscriberModel.find({});
+    for (const subs of subscribers) {
+      const subsSkills = subs.skills;
+      const jobWithMatchingSkills = await this.jobModel.find({
+        skills: { $in: subsSkills },
+      });
+      if (jobWithMatchingSkills?.length) {
+        const jobs = jobWithMatchingSkills.map((item) => {
+          return {
+            name: item.name,
+            company: item.company.name,
+            salary:
+              `${item.salary}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' đ',
+            skills: item.skills,
+          };
+        });
+
+        await this.mailerService.sendMail({
+          to: 'duchaivu1997@gmail.com',
+          from: '"Support Team" <support@example.com>', // override default from
+          subject: 'Welcome to Nice App! Confirm your Email',
+          template: 'new-job', //'<b>welcome bla bla</b>', // HTML body content
+          context: {
+            receiver: subs.name,
+            jobs: jobs,
+          },
+        });
+      }
+    }
   }
 }
